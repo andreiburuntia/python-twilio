4e14d084118575d5f0213d2902fe67ec6ebdfa5f
astefan <stefan.avram@irian.ro>
2017-03-11 23:30:22 +0000

    switching

--- server.py
+++ server.py
@@ -9,19 +9,16 @@ def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
-
     # Start our TwiML response
     resp = twiml.Response()
 
     if body is not None:
         arguments = body.split("\s")
 
-        if arguments[0] == 'location':
+        if arguments[0] == 'Location':
             s=body
-            start_loc=s[s.find("(")+1:s.find(")")]
-            a=s.split(",")[1]
-            end_loc=a[a.find("(")+1:a.find(")")]
-            resp.message(start_loc+" - "+end_loc)
+            start_location=s[s.find("(") + 1:s.find(")")]
+            resp.message("start location: "+start_location)
 
         elif arguments[0] == 'twitter':
             resp.message("Hi -1!")
