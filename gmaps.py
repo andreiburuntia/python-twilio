a0fa15a9c562d956e905acdc5a1b4b2d45bb7935
Andrei Buruntia <andreiburuntia@gmail.com>
2017-03-11 21:21:04 +0000

    finished send sms functionality, little refactoring

--- gmaps.py
+++ gmaps.py
@@ -4,34 +4,65 @@ import json
 import re
 from twilio.rest import TwilioRestClient
 
-mykey='AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8'
+# hardcoded, from twilio.com/console or whatever
+mykey = 'AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8'
 gmaps = googlemaps.Client(key='AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8')
 
-start='manchaster airport'
-end='mediacityuk'
-
-directions_result=gmaps.directions(start,end,mode="driving",departure_time=datetime.now())
-with open('data.txt', 'w') as outfile:
-    json.dump(directions_result,outfile)
 
 def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
 
-directions_list=[]
 
-for single_direction in directions_result[0]['legs'][0]['steps']:
-    directions_list.append(cleanhtml(single_direction['html_instructions']))
-    print (cleanhtml(single_direction['html_instructions']))
+def fetch_directions(start, end, mode, departure_time):
+    result_tuple = ()
+    directions_list = []
+    directions_result = gmaps.directions(
+        start, end, mode=mode, departure_time=departure_time)
+    result_tuple += (directions_result[0]['legs'][0]['distance']['text'], )
+    result_tuple += (directions_result[0]['legs'][0]['duration']['text'], )
+    for single_direction in directions_result[0]['legs'][0]['steps']:
+        directions_list.append(
+            cleanhtml(single_direction['html_instructions']))
 
-account_sid = "AC9a8405724afb8b609912054251250dda" # Your Account SID from www.twilio.com/console
-auth_token  = "a9658b8cea6a3daa57c62583e8256840"  # Your Auth Token from www.twilio.com/console
+    result_tuple += (directions_list, )
+    # tuple of ("distance","duration",[list of directions])
+    return result_tuple
 
-client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(body="Hello from Python",
-    to="+447375531812",    # Replace with your phone number
-    from_="+442033899587") # Replace with your Twilio number
+def send_sms(content, to_number, from_number):
+    # Your Account SID from www.twilio.com/console
+    account_sid = "AC9a8405724afb8b609912054251250dda"
+    # Your Auth Token from www.twilio.com/console
+    auth_token = "a9658b8cea6a3daa57c62583e8256840"
 
-print(message.sid)
+    client = TwilioRestClient(account_sid, auth_token)
+
+    message = client.messages.create(body=content,
+                                     to=to_number,    # Replace with your phone number
+                                     from_=from_number)  # Replace with your Twilio number
+
+
+def concat_with_new_line(tuple):
+    count = 1
+    result = ""
+    result += "Distance: " + tuple[0] + "\n\n"
+    result += "Duration: " + tuple[1] + "\n\n"
+    for single_direction in tuple[2]:
+        single_direction = single_direction.replace(
+            "&amp;", "&")  # escape weird char
+        single_direction = single_direction.replace(
+            "⛉ ", "")  # escape weird char
+        result += str(count) + ". " + single_direction + "\n\n"
+        count += 1
+    return result
+
+# main
+
+
+test_directions = fetch_directions(
+    "manchester airport", "manchester mediacityuk", "driving", datetime.now())
+test_content = concat_with_new_line(test_directions)
+# print(test_content)
+send_sms(test_content,"+447375531812","+442033899587")
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect
-import twilio.twiml
+from twilio import twiml
 
 app = Flask(__name__)
 
@@ -8,8 +8,25 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
-    resp = twilio.twiml.Response()
+    resp = twiml.Response()
     resp.message("Hello, Mobile Monkey")
+    return str(resp)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = twiml.Response()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
     return str(resp)
 
 
