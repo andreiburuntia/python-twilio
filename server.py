6bf516625e60b087b3bc0f231bf0932283766cda
Andrei Buruntia <andreiburuntia@gmail.com>
2017-03-12 00:27:35 +0000

    location stuff

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from twilio import twiml
 app = Flask(__name__)
 
 
-
-
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -19,25 +17,16 @@ def incoming_sms():
         arguments = body.split("\s")
 
         if arguments[0] == 'location':
-            resp.message("Hi -0!")
+            s=body
+            start_loc=s[s.find("(")+1:s.find(")")]
+            a=s.split(",")[1]
+            end_loc=a[a.find("(")+1:a.find(")")]
+            resp.message(start_loc+" - "+end_loc)
 
         elif arguments[0] == 'twitter':
             resp.message("Hi -1!")
 
     return str(resp)
-
-@app.route("/sms", methods=['GET', 'POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
-    body = request.values.get('Body', None)
-
-    # Start our TwiML response
-    resp = twiml.Response()
-
-
-    return str(resp)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
