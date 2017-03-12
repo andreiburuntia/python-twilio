1b81034754f42442bc30cf0858ba9d0a0bfeb3b0
Andrei Buruntia <andreiburuntia@gmail.com>
2017-03-12 01:00:57 +0000

    finished location, still not working for some reason

--- gmaps.py
+++ gmaps.py
@@ -3,66 +3,66 @@ from datetime import datetime
 import json
 import re
 from twilio.rest import TwilioRestClient
-
-# hardcoded, from twilio.com/console or whatever
-mykey = 'AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8'
-gmaps = googlemaps.Client(key='AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8')
-
-
-def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    return cleantext
-
-
-def fetch_directions(start, end, mode, departure_time):
-    result_tuple = ()
-    directions_list = []
-    directions_result = gmaps.directions(
-        start, end, mode=mode, departure_time=departure_time)
-    result_tuple += (directions_result[0]['legs'][0]['distance']['text'], )
-    result_tuple += (directions_result[0]['legs'][0]['duration']['text'], )
-    for single_direction in directions_result[0]['legs'][0]['steps']:
-        directions_list.append(
-            cleanhtml(single_direction['html_instructions']))
-
-    result_tuple += (directions_list, )
-    # tuple of ("distance","duration",[list of directions])
-    return result_tuple
-
-
-def send_sms(content, to_number, from_number):
-    # Your Account SID from www.twilio.com/console
-    account_sid = "AC9a8405724afb8b609912054251250dda"
-    # Your Auth Token from www.twilio.com/console
-    auth_token = "a9658b8cea6a3daa57c62583e8256840"
-
-    client = TwilioRestClient(account_sid, auth_token)
-
-    message = client.messages.create(body=content,
-                                     to=to_number,    # Replace with your phone number
-                                     from_=from_number)  # Replace with your Twilio number
-
-
-def concat_with_new_line(tuple):
-    count = 1
-    result = ""
-    result += "Distance: " + tuple[0] + "\n\n"
-    result += "Duration: " + tuple[1] + "\n\n"
-    for single_direction in tuple[2]:
-        single_direction = single_direction.replace(
-            "&amp;", "&")  # escape weird char
-        single_direction = single_direction.replace(
-            "⛉ ", "")  # escape weird char
-        result += str(count) + ". " + single_direction + "\n\n"
-        count += 1
-    return result
-
-# main
-
-
-test_directions = fetch_directions(
-    "manchester airport", "manchester mediacityuk", "driving", datetime.now())
-test_content = concat_with_new_line(test_directions)
-# print(test_content)
-send_sms(test_content,"+447375531812","+442033899587")
+#
+# # hardcoded, from twilio.com/console or whatever
+# mykey = 'AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8'
+# gmaps = googlemaps.Client(key='AIzaSyDvcfhT988-hv6TFsMey1K_q84hfAh9Nm8')
+#
+#
+# def cleanhtml(raw_html):
+#     cleanr = re.compile('<.*?>')
+#     cleantext = re.sub(cleanr, '', raw_html)
+#     return cleantext
+#
+#
+# def fetch_directions(start, end, mode, departure_time):
+#     result_tuple = ()
+#     directions_list = []
+#     directions_result = gmaps.directions(
+#         start, end, mode=mode, departure_time=departure_time)
+#     result_tuple += (directions_result[0]['legs'][0]['distance']['text'], )
+#     result_tuple += (directions_result[0]['legs'][0]['duration']['text'], )
+#     for single_direction in directions_result[0]['legs'][0]['steps']:
+#         directions_list.append(
+#             cleanhtml(single_direction['html_instructions']))
+#
+#     result_tuple += (directions_list, )
+#     # tuple of ("distance","duration",[list of directions])
+#     return result_tuple
+#
+#
+# def send_sms(content, to_number, from_number):
+#     # Your Account SID from www.twilio.com/console
+#     account_sid = "AC9a8405724afb8b609912054251250dda"
+#     # Your Auth Token from www.twilio.com/console
+#     auth_token = "a9658b8cea6a3daa57c62583e8256840"
+#
+#     client = TwilioRestClient(account_sid, auth_token)
+#
+#     message = client.messages.create(body=content,
+#                                      to=to_number,    # Replace with your phone number
+#                                      from_=from_number)  # Replace with your Twilio number
+#
+#
+# def concat_with_new_line(tuple):
+#     count = 1
+#     result = ""
+#     result += "Distance: " + tuple[0] + "\n\n"
+#     result += "Duration: " + tuple[1] + "\n\n"
+#     for single_direction in tuple[2]:
+#         single_direction = single_direction.replace(
+#             "&amp;", "&")  # escape weird char
+#         single_direction = single_direction.replace(
+#             "⛉ ", "")  # escape weird char
+#         result += str(count) + ". " + single_direction + "\n\n"
+#         count += 1
+#     return result
+#
+# # main
+#
+#
+# test_directions = fetch_directions(
+#     "manchester airport", "manchester mediacityuk", "driving", datetime.now())
+# test_content = concat_with_new_line(test_directions)
+# # print(test_content)
+# send_sms(test_content,"+447375531812","+442033899587")
--- server.py
+++ server.py
@@ -1,8 +1,44 @@
 from flask import Flask, request, redirect
+import twilio.twiml
+import datetime
+import gmaps
 from twilio import twiml
 
 app = Flask(__name__)
 
+
+# @app.route("/", methods=['GET', 'POST'])
+# @app.route("/sms", methods=['GET', 'POST'])
+# def hello_monkey():
+#     """Respond to incoming calls with a simple text message."""
+#
+#     resp = twiml.Response()
+#     resp.message("Hello, Mobile Monkey")
+#     return str(resp)
+
+
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+    body = body.upper()
+    # Start our TwiML response
+    resp = twiml.Response()
+
+    if body is not None:
+        arguments = body.split(" ")
+        if arguments[0] == 'Location':
+            # s=body
+            # start_loc=s[s.find("(")+1:s.find(")")]
+            # a=s.split(",")[1]
+            # end_loc=a[a.find("(")+1:a.find(")")]
+            # directions=gmaps.fetch_directions(start_loc,end_loc,"driving",datetime.now())
+            # response_content=gmaps.concat_with_new_line(directions)
+            # resp.message(str(response_content))
+        # elif arguments[0] == 'Twitter':
+            resp.message("Hi -1!")
+
+    return str(resp)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -19,7 +55,9 @@ def incoming_sms():
             start_loc=s[s.find("(")+1:s.find(")")]
             a=s.split(",")[1]
             end_loc=a[a.find("(")+1:a.find(")")]
-            resp.message(start_loc+" - "+end_loc)
+            directions=gmaps.fetch_directions(start_loc,end_loc,"driving",datetime.now())
+            response_content=gmaps.concat_with_new_line(directions)
+            resp.message(str(response_content))
 
         elif arguments[0] == 'Twitter':
             resp.message("Hi -1!")
